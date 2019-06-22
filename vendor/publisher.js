620bd7168d659b6a73d0a14e6e263d079062ab04
Shubhendra Singh <withshubh@gmail.com>
2019-06-23 02:08:53 +0530

    new

---
+++ vendor/publisher.js
@@ -0,0 +1,60 @@
+function setupAgoraSDKforPublisher(channel){
+    if(!AgoraRTC.checkSystemRequirements()) {
+        alert("Your browser does not support WebRTC!");
+      }
+      var client, localStream;
+      client = AgoraRTC.createClient({mode: 'live',codec: "h264"});
+        client.init('7041dbc051304a33b10989bedb5a846d', function () {
+          console.log("AgoraRTC client initialized");
+          var channel_key = null;
+          client.join(channel_key, channel, null, function(uid) {
+            console.log("User " + uid + " join channel successfully");      
+              localStream = AgoraRTC.createStream({
+                    streamID: uid,
+                    audio: false,
+                    video: false,
+                    screen: true,
+                    mediaSource: 'screen' // 'screen', 'application', 'window'
+                  });
+      
+              // The user has granted access to the camera and mic.
+              localStream.on("accessAllowed", function() {
+                console.log("accessAllowed");
+              });
+      
+              // The user has denied access to the camera and mic.
+              localStream.on("accessDenied", function() {
+                console.log("accessDenied");
+              });
+      
+              localStream.init(function() {
+                console.log("getUserMedia successfully");
+                client.publish(localStream, function (err) {
+                  console.log("Publish local stream error: " + err);
+                });
+      
+                client.on('stream-published', function (evt) {
+                  console.log("Publish local stream successfully");
+                });
+              }, function (err) {
+                console.log("getUserMedia failed", err);
+              });
+        
+          }, function(err) {
+            console.log("Join channel failed", err);
+          });
+        }, function (err) {
+          console.log("AgoraRTC client init failed", err);
+        });
+        channelKey = "";
+        client.on('error', function(err) {
+          console.log("Got error msg:", err.reason);
+          if (err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+            client.renewChannelKey(channelKey, function(){
+              console.log("Renew channel key successfully");
+            }, function(err){
+              console.log("Renew channel key failed: ", err);
+            });
+          }
+        });
+}
---
+++ vendor/subscriber.js
@@ -0,0 +1,51 @@
+function setupAgoraSDKforSubscriber(channel){
+    if(!AgoraRTC.checkSystemRequirements()) {
+        alert("Your browser does not support WebRTC!");
+      }
+      
+      var client, localStream, camera, microphone;
+      client = AgoraRTC.createClient({mode: 'live',codec: "h264"});
+      client.init('7041dbc051304a33b10989bedb5a846d', function () {
+          console.log("AgoraRTC client initialized");
+          var channel_key = null;
+          client.join(channel_key, channel, null, function(uid) {
+            console.log("User " + uid + " join channel successfully");      
+      
+        
+          }, function(err) {
+            console.error("Join channel failed", err);
+          });
+        }, function (err) {
+          console.error("AgoraRTC client init failed", err);
+        });
+      client.on('stream-added', function (evt) {
+      var stream = evt.stream;
+      console.log("New stream added: " + stream.getId());
+    
+      client.subscribe(stream, function (err) {
+        console.log("Subscribe stream failed", err);
+      });
+    });
+    client.on('stream-subscribed', function (evt) {
+      var remoteStream = evt.stream;
+      console.log("Subscribe remote stream successfully: " + remoteStream.getId());
+      remoteStream.play('agora_local');
+    })
+    client.on('peer-leave', function(evt) {
+        console.log('Peer Left');
+        alert('Event has ended');
+    });
+        
+        channelKey = "";
+        client.on('error', function(err) {
+          console.log("Got error msg:", err.reason);
+          if (err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+            client.renewChannelKey(channelKey, function(){
+              console.log("Renew channel key successfully");
+            }, function(err){
+              console.error("Renew channel key failed: ", err);
+            });
+          }
+        });
+    
+}
